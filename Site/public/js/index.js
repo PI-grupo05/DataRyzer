27a2e0f26c21d7495762a88857f324f79c840216
FabioHKC <fabio.cesare@sptech.school>
2025-03-09 02:39:14 -0300

    Adicionando responsividade ao site

---
+++ Site/public/js/index.js
@@ -0,0 +1,16 @@
+menuFechado = true
+
+function abrirMenu(){
+    menu = document.getElementsByClassName('nav__menu')
+    header = document.getElementsByClassName('header')
+    if(menuFechado == true){
+        menu[0].style.display = 'flex'
+        header[0].style.transition = '0.5s'
+        header[0].style.height = '35%'
+        menuFechado = false
+    }else{
+        menu[0].style.display = 'none'
+        header[0].style.height = '10%'
+        menuFechado = true
+    }
+}
