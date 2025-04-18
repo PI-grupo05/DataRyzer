ef16b09c4c6b5d2ce7f0670cee890df12b27060c
FabioHKC <fabio.cesare@sptech.school>
2025-04-18 18:23:47 -0300

    Arrumando menu hamburguer

--- homepage-angular/src/app/components/header/header.component.ts
+++ homepage-angular/src/app/components/header/header.component.ts
@@ -9,22 +9,16 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
    menuFechado: boolean = true
-/*
- abrirMenu(){
-    const menu = document.getElementsByClassName('nav__menu')
-    const header = document.getElementsByClassName('header')
-    if(this.menuFechado == true){
-        menu[0].style.display = 'flex'
-        header[0].style.transition = '0.5s'
-        header[0].style.height = '30%'
-        this.menuFechado = false
-    }else{
-        menu[0].style.display = 'none'
-        header[0].style.height = '10%'
-        this.menuFechado = true
+
+    abrirMenu(){
+        if(this.menuFechado == true){
+            this.menuFechado = false
+        }else{
+            this.menuFechado = true
+        }
     }
-}
- abrirLogin(){
-    window.location.href = "login.html"
-} */
+
+    abrirLogin(){
+        window.location.href = "login.html"
+    }
 }
