bf3978aa26cd54426f4ad944861a9477a696f2a4
Felipe Lima Vieira <felipe.vieira@sptech.school>
2025-05-30 22:56:22 -0300

    feat: lógica de botões de sibar
    Vai aparecer apenas os botões que o usuario tem acesso

--- Site/public/js/sidebar.js
+++ Site/public/js/sidebar.js
@@ -17,3 +17,40 @@ function abrirMenu(){
     menuAberto = true
   }
 }
+
+
+window.onload = verificarTipoUsuario
+function verificarTipoUsuario() {
+
+  var sidebar = document.getElementById("menuLateralSecoes")
+  var tipoUsuario = sessionStorage.TIPO_USUARIO
+  if(tipoUsuario == "MASTER") {
+    sidebar.innerHTML = `<a href="./menu.html">
+    <img src="./assets/imgs/iconeDash.png" alt="">
+    <span>Menu incial</span>
+  </a>
+  <a href="./dashGeral.html">
+    <img src="./assets/imgs/icon_grafico.svg" alt="">
+    <span>Dashboard</span>
+  </a>
+  <a href="./unidadeConsumidora.html">
+    <img src="./assets/imgs/iconeEmpresas.png" alt="">
+    <span>Unidades Consumidoras</span>
+  </a>
+  <a href="./historico_interrupcao.html">
+    <img src="./assets/imgs/historico_icon.png" alt="">
+    <span>historico de interrupções</span>
+  </a> `
+  console.log("Inseri essa merda")
+  } else {
+    sidebar.innerHTML =
+    `<a href="./menu.html">
+    <img src="./assets/imgs/iconeDash.png" alt="">
+    <span>Menu incial</span>
+    </a>
+    <a href="./dashEspecifica.html">
+      <img src="./assets/imgs/icon_grafico.svg" alt="">
+      <span>Dashboard</span>
+    </a>`
+  }
+}
